b96bc28308ca480fef13f07cb4ce8045bc32f037
Forlify <forlify@gmail.com>
2020-07-11 16:55:18 +0200

    issues resolved, changes with contact section

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 // REQUIRE ////////////////////////////////////////////////////////////////////
 
 const express = require('express')
-const validator = require('validator')
 const bodyParser = require('body-parser')
-const request = require('request')
-
-const sendEmail = require('./public/resources/nodejs/email')
 
 const app = express()
 
@@ -16,47 +12,10 @@ app.set('views', __dirname + '/public')
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
-app.use(express.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
 // GET / POST ////////////////////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
     res.render('index.html')
-})
-
-app.post('/submit-form', (req, res) => {
-    const name = req.body.name
-    const email = req.body.email
-    const message = req.body.message
-
-    if(
-        !validator.isEmail(email) ||
-        name.length > 50 || 
-        email.length > 50 || 
-        message.length > 800 
-    ) return res.json({"success": false, "msg":"Wrong input."})
-    
-    if(
-        req.body.captcha === undefined ||
-        req.body.captcha === '' ||
-        req.body.captcha === null 
-    ) return res.json({"success": false, "msg":"Please select captcha."})
-        
-    const secretKey = process.env.RECAPTCHA_API_KEY 
-    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
-    
-    request(verifyUrl, (err, response, body) => {
-        body = JSON.parse(body)
-
-        if(body.success !== undefined && !body.success) {
-            return res.json({"success": false, "msg":"Captcha verification failed."})
-        }
-
-        sendEmail(name,email,message)
-
-        return res.json({"success": true, "msg":"Sent successfully!"})
-    })
 })
 
 app.get('*', (req, res) => {
@@ -65,7 +24,7 @@ app.get('*', (req, res) => {
 
 // LISTEN ////////////////////////////////////////////////////////////////////
 
-app.listen(50000, () => {
-    console.log(`Server running at http://blazejkustra:50000/`)
+app.listen(3000, () => {
+    console.log(`Server running at http://localhost:3000`)
 })
 
